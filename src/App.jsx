1146533d9a62bfb5aa587e92034c480dd473e481
MS. CHAYADA MUANGBOONSRI <66130500838@st.sit.kmutt.ac.th>
2024-06-12 15:29:54 +0700

    SA Technical Onboard web done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
 
 import About from "./Components/About";
@@ -14,25 +8,16 @@ import Portfolio from "./Components/Portfolio";
 
 import "./styles.css";
 
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chayada Muangboonsri",
+  title: "CS@SIT KMUTT Student",
+  email: "chayada.mu2@gmail.com",
+  gitHub: "galilynnn",
+  instagram: "galilynnn_",
+  linkedIn: "galilynnn",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
